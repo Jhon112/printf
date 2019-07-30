d26b4d02b1f0b4284a955d623f3a8bf0bd6adad0
juanquinterodev <896@holbertonschool.com>
2019-07-30 23:33:35 +0000

    Fix print_numbers function and checkflag fn

--- checkFlag.c
+++ checkFlag.c
@@ -9,46 +9,46 @@
  *
  * Return: 0 if error, 1 otherwise
  */
+
 int checkFlag(int *contador, const char **format, va_list chars)
 {
-	char caracter;
-	char *string;
-	int length;
-	int integer;
-	switch (**format)
-	{
-	case 'c':
-		caracter = (char)va_arg(chars, int);
-		write(1, &caracter, 1);
-		*contador += 1;
-		*format += 1;
-		return (1);
-	case 's':
-		string = va_arg(chars, char *);
-		if (string == NULL)
-			return (0);
-		length = strlength(string);
-		write(1, string, length);
-		*contador += length;
-		*format += 1;
-		return (1);
-	case '%':
-		write(1, *format, 1);
-		*contador += 1;
-		*format += 1;
-		return (1);
+char caracter;
+char *string;
+int length;
+unsigned int integer;
 
-	case 'd':
-        case 'i':
-                integer = va_arg(chars, int);
-                print_numbers(integer);
-                length = length_number(integer);
-                *contador += length;
-                *format += 1;
-                return (1);
-	default:
-		write(1, *format - 1, 1);
-		*contador += 1;
-		return (0);
-	}
+switch (**format)
+{
+case 'c':
+caracter = (char)va_arg(chars, int);
+write(1, &caracter, 1);
+*contador += 1;
+*format += 1;
+return (1);
+case 's':
+string = va_arg(chars, char *);
+if (string == NULL)
+return (0);
+length = strlength(string);
+write(1, string, length);
+*contador += length;
+*format += 1;
+return (1);
+case '%':
+write(1, *format, 1);
+*contador += 1;
+*format += 1;
+return (1);
+case 'd':
+case 'i':
+integer = va_arg(chars, int);
+integer = print_number(integer, 1);
+*contador = *contador + integer;
+*format += 1;
+return (1);
+default:
+write(1, *format - 1, 1);
+*contador += 1;
+return (0);
+}
 }
--- print_numbers.c
+++ print_numbers.c
@@ -1,31 +1,44 @@
 #include "holberton.h"
 /**
-* print_numbers - print number
-* @n: integer
-*
-* Return: nothing
+* print_number - Prints an integer.
+* @numb: integer to be printed.
+* @value: If number is signed or unsigned.
+* Return: Number of chars printed.
 */
-void print_numbers(int n)
+unsigned int print_number(int numb, short value)
 {
-unsigned int up = 0;
+	unsigned int result;
+	unsigned int diference = 1;
+	unsigned int ch;
+	unsigned int chars = 0;
 
-if (n % 10 < 0)
-{
-_putchar('-');
-up = -n;
+	if (value)
+	{
+		if (numb < 0)
+		{
+			numb *= -1;
+			_putchar('-');
+			chars++;
+		}
+		ch = numb;
+	}
+	else
+	{
+		ch = (unsigned int)numb;
+	}
+	while (ch / 10)
+	{
+		diference *= 10;
+		ch /= 10;
+	}
+	_putchar(ch + '0');
+	chars++;
+	while (diference >= 10)
+	{
+		result = numb % diference;
+		diference /= 10;
+		_putchar(result / diference + '0');
+		chars++;
+	}
+	return (chars);
 }
-else
-{
-up = n;
-}
-if (up < 10)
-{
-_putchar('0' +  (up % 10));
-}
-else
-{
-print_numbers(up / 10);
-_putchar('0' + up % 10);
-}
-}
-
